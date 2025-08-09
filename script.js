5f2479006704625f7c335b78e4be3a9fc5311788
atheneum27 <alatheneum@gmail.com>
2025-08-09 21:15:03 +0700

    Update script.js

--- script.js
+++ script.js
@@ -177,3 +177,29 @@ setupSignatureCanvas();
 populateNameDropdown();
 document.getElementById('contact-form').reset();
 signatureCtx.clearRect(0, 0, signatureCanvas.width, signatureCanvas.height);
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDApxCwdqXURI9AXdNvwRK5LDwqDPMXe_I",
+  authDomain: "atheneumsync.firebaseapp.com",
+  databaseURL: "https://atheneumsync-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "atheneumsync",
+  storageBucket: "atheneumsync.firebasestorage.app",
+  messagingSenderId: "383620444372",
+  appId: "1:383620444372:web:fc408b459512629d0e8b41",
+  measurementId: "G-Y4MX263QBM"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
